66eb98009d5c402d9d26777319187dd3d3190e6a
Simran Singh <siimransiingh08@gmail.com>
2024-03-07 11:52:00 +0530

    mongo db connection

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from "morgan"
+import connect from './database/connection.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable(morgan('x-powered-by')); // less hackers knowabout our stack
+
+const port = 8080;
+
+app.get('/',(req,res)=> {
+    res.status(201).json("home request")
+})
+
+connect().then(()=>{
+    try {
+        app.listen(port,()=>{
+            console.log("server connected")
+        })
+    } catch (error) {
+        console.log("cannot connectto server")
+    }
+}).catch(error=>{
+    console.log("invalid db connection ")
+})
+
