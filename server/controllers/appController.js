9b8d227cd65c68862a29ea626bd9007b93bef1ff
Simran Singh <siimransiingh08@gmail.com>
2024-03-07 20:09:57 +0530

    /login config

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,6 +1,26 @@
 import { response } from "express"
 import UserModel from "../model/User.model.js"
 import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken';
+import 'dotenv/config' 
+
+
+/** middleware for verify user */
+export async function verifyUser(req, res, next){
+    try {
+        
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        // check the user existance
+        let exist = await UserModel.findOne({ username });
+        if(!exist) return res.status(404).send({ error : "Can't find User!"});
+        next();
+
+    } catch (error) {
+        return res.status(404).send({ error: "Authentication Error"});
+    }
+}
+
 /** POST: http://localhost:8080/signup 
  * @param : {
   "username" : "example123",
@@ -78,8 +98,45 @@ export async function register(req, res) {
 }
 */
 
-export async function login(req, res) {
-    res.json("login route")
+export async function login(req,res){
+   
+    const { username, password } = req.body;
+
+    try {
+        
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
+
+                        if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
+
+                        // create jwt token
+                        const token = jwt.sign({
+                                        userId: user._id,
+                                        username : user.username
+                                    }, process.env.JWT_SECRET , { expiresIn : "24h"});
+
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });                                    
+
+                    })
+                    .catch(error =>{
+                       
+                        return res.status(400).send({ error: "Password does not Match"})
+                        
+                    })
+            })
+            .catch( error => {
+                return res.status(404).send({ error : "Username not Found"});
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error});
+    }
 }
 
 /** GET: http://localhost:8080/user/example123 */
