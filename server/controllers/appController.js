de866f4bd44b908644314d6791559b475dbd059d
Simran Singh <siimransiingh08@gmail.com>
2024-03-07 20:18:57 +0530

    getUser config

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -140,8 +140,29 @@ export async function login(req,res){
 }
 
 /** GET: http://localhost:8080/user/example123 */
-export async function getUser(req, res) {
-    res.json("getuser route")
+export async function getUser(req,res){
+    
+    const { username } = req.params;
+
+    try {
+        
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            /** remove password from user */
+            // mongoose return unnecessary data with object so convert it into json
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
+
 }
 
 /** PUT: http://localhost:8080/api/updateuser 
