59b45a985ddfae1c75b040a34acc3287cc85cc68
Simran Singh <siimransiingh08@gmail.com>
2024-03-08 16:52:25 +0530

    register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
 import avatar from '../assets/profile.png';
 import styles from '../styles/Username.module.css';
 import { useFormik } from 'formik';
-import { Toaster } from 'react-hot-toast';
+import toast,{ Toaster } from 'react-hot-toast';
 import { registerValidation } from '../helper/validate';
 import convertToBase64 from './../helper/convert';
+import { registerUser } from '../helper/helper';
 
 export default function Register() {
+  const navigate = useNavigate();
   const [file,setFile] = useState();
   const formik = useFormik({
     initialValues: {
@@ -18,9 +20,16 @@ export default function Register() {
     validate: registerValidation,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit: async values => {
-      values = await Object.assign(values,{profile:file || ''})
-      console.log(values)
+    onSubmit : async values => {
+      values = await Object.assign(values, { profile : file || ''})
+      let registerPromise = registerUser(values)
+      toast.promise(registerPromise, {
+        loading: 'Creating...',
+        success : <b>Register Successfully...!</b>,
+        error : <b>Could not Register.</b>
+      });
+
+      registerPromise.then(function(){ navigate('/')});
     }
   })
 
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -69,7 +69,7 @@ export async function profileValidation(values){
 function emailVerify(error={}, values){
     if(!values.email){
         error.email = toast.error("Email required")
-    }   else if(values.email.includes("")){
+    }   else if(values.email.includes(" ")){
         error.email=toast.error("Wrong email")
     } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
         error.email = toast.error("Invalid email address")
